0b83c1b3e90236a1b6055ef50bc1d5fbfe4a6918
SGKoishi <i@sgkoi.dev>
2020-05-22 00:00:37 -0700

    Trim leading v in version string

--- prepare.py
+++ prepare.py
@@ -8,7 +8,7 @@ urllib.request.urlretrieve("https://api.github.com/repos/Pryaxis/TShock/releases
 lj = json.loads(open("gh.json", encoding="utf-8").read())
 url = lj[0]["assets"][0]["browser_download_url"]
 urllib.request.urlretrieve(url, "tshock.zip")
-with zipfile.ZipFile("tshock.zip", 'r') as zip_ref:
+with zipfile.ZipFile("tshock.zip", "r") as zip_ref:
     zip_ref.extractall("target")
 
 os.mkdir("binary")
@@ -22,7 +22,7 @@ for subdir, dirs, files in os.walk("target"):
             if file not in excludes:
                 shutil.copy(filepath, "binary" + os.sep + file)
 
-nuspec = open("template.nuspec", encoding="utf-8").read().replace("VERSIONPLACEHOLDER", lj[0]["tag_name"]).replace("NAMEPLACEHOLDER", lj[0]["name"])
+nuspec = open("template.nuspec", encoding="utf-8").read().replace("VERSIONPLACEHOLDER", lj[0]["tag_name"].lstrip("v")).replace("NAMEPLACEHOLDER", lj[0]["name"])
 
 with open("tshock.nuspec", "w") as f:
     f.write(nuspec)
